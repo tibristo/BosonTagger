8097b0a1a7086b44b52a70d45e5248253dcd8a09
tibristo <timmybristowguy@gmail.com>
2015-11-22 11:29:00 +0000

    Fixed some errors in featureImportances.py

--- featureImportances.py
+++ featureImportances.py
@@ -5,7 +5,7 @@ import sys
 
 in_file = sys.argv[1]
 if len(sys.argv) > 2:
-    out_file = sys.arv[2]
+    out_file = sys.argv[2]
 else:
     out_file = in_file.replace('.pbz2','_featureImportances.txt')
 
@@ -20,8 +20,9 @@ taggers = model.taggers
 features = model.feature_importances
 sorted_idx = np.argsort(features)[::-1]
 outf = open(out_file,'w')
+max_feature = features[sorted_idx[0]]
 for f in range(len(features)):
-    print("%d. feature %s (%f)" % (f + 1, taggers[sorted_idx[f]], features[sorted_idx[f]]))
-    outf.write("%d. feature %s (%f)" % (f + 1, taggers[sorted_idx[f]], features[sorted_idx[f]])+ '\n')
+    print("%d. feature %s (%f)" % (f + 1, taggers[sorted_idx[f]], 100*features[sorted_idx[f]]/max_feature))
+    outf.write("%d. feature %s (%f)" % (f + 1, taggers[sorted_idx[f]], 100*features[sorted_idx[f]]/max_feature)+ '\n')
 outf.close()
 #get_ipython().magic(u'save featureImportances')
